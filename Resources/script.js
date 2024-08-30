67f8fc701024143b17771b79acf46209805c77a0
ElfinClient <123729397+ElfinClient@users.noreply.github.com>
2024-08-30 13:24:18 +1000

    Update script.js
    added improvements

--- Resources/script.js
+++ Resources/script.js
@@ -1,11 +1,4 @@
-const switchModeBtn = document.querySelector('mode-switch')
-
-switchModeBtn.addEventListener('click', () => {
-    document.body.classList.toggle()
-})
-
 function myFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
-    element.classList.toggle(".about-me-dark-mode");
  }
